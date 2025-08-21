1dc4c22e56f1f4857ef07a35c33e8b0ab93ae7e5
Hiorrana Nicolly <nicollyhiorrana3@gmail.com>
2025-08-21 19:10:44 -0300

    ajuste na validação de positivos

--- lista1/exercicio15.py
+++ lista1/exercicio15.py
@@ -2,11 +2,26 @@
 # D = (R+S)/2
 # R = (A+B)**2 e S = (B+C)**2
 
-a = int(input("Número A: "))
-b = int(input("Número B: "))
-c = int(input("Número C: "))
+def main():
+    a = ler_numero('Número A (inteiro e positivo): ')
+    b = ler_numero('Número B (inteiro e positivo): ')
+    c = ler_numero('Número C (inteiro e positivo) ')
 
-r = (a+b)**2
-s = (b+c)**2
+    r = (a+b)**2
+    s = (b+c)**2
 
-print(f"A expressão vale: {(r+s)/2}")
+    print(f"A expressão vale: {(r+s)/2:.2f}")
+    
+#função para ler a entrada e garantir que o número é positivo
+def ler_numero(msg):
+    while True:
+        try:
+            num = int(input(msg))
+            if num > 0:
+                return num
+            else:
+                print('O número deve ser positivo. Tente Novamente!')
+        except ValueError:
+            print(f'Digite apenas números')
+
+main()
